76dc090b80c6ff3990963ebb2b61ca8f42379f2c
Khelsai Rajwade <khelsai@gamil.com>
2024-05-26 12:58:10 +0530

    navbar added pointer and change font size of text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex justify-end bg-sky-300 p-4'>
-          <ul className=' sm:w-1/2 lg:1/2 flex justify-evenly mt-3 text-white text-lg font-medium font-serif  gap-2 '>
+          <ul className=' sm:w-1/2 cursor-pointer lg:1/2 flex justify-evenly mt-3 text-white text-lg font-medium font-serif  gap-2 '>
               <li>Our Customer</li>
               <li>spArts Advantage</li>
               <li>Contact Us</li>
